c95d338e8d59fb68afbe8819985358059a48a997
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-07-06 18:56:26 +0430

    init projects

---
+++ src/main/java/com/example/bankcustomerservice/BankCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankcustomerservice/BankCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
